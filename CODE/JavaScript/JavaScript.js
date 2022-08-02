69eb69ca2d59831e99993ef7bfccfe1ac58c2bf7
G6H4 <105165515+g6h4@users.noreply.github.com>
2022-08-02 20:40:16 +0300

    Add files via upload

---
+++ CODE/JavaScript/JavaScript.js
@@ -0,0 +1,55 @@
+function ScrollTo(y) {
+    window.scrollTo({
+        top: y,
+        behavior: 'smooth'
+    })
+};
+
+// circle variable
+let circle = document.querySelector('.circle');
+
+// circle mouse follower
+const moveCircle = (e) => {
+    circle.style.left = `${e.pageX}px`;
+    circle.style.top = `${e.pageY}px`
+}
+const addPulse = () => {
+    circle.classList.toggle('active-theme-1');
+}
+
+window.addEventListener('mousemove' , moveCircle);
+window.addEventListener('mousedown' , addPulse);
+window.addEventListener('mouseup' , addPulse);
+
+// circle themes
+window.addEventListener('keydown' , e => {
+    // other shortcuts
+    if (e.key === "`") {
+        let username = prompt("Please enter your username");
+
+        if (username === null) {
+            alert("Hello!");
+        } else {
+            alert("Hello " + username + "!");
+        }
+    }
+
+    // navigation shortcuts
+    if (e.key === "1") {
+        location.href = '#Welcome-Screen'
+    }
+    if (e.key === "2") {
+        location.href = '#Skills-Screen'
+    }
+    if (e.key === "3") {
+        location.href = '#About-Me-Screen'
+    }
+    if (e.key === "4") {
+        location.href = '#Projects-Screen'
+    }
+
+    // circle themes
+    if (e.ctrlKey) {
+        circle.classList.toggle('active-theme-1');
+    }
+})
